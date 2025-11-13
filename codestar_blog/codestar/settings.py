be1028ad63297d2826fd62e2976719b2873e9916
Brianon97 <brianon1997@gmail.com>
2025-11-13 10:42:04 +0000

    Fix STATIC_ROOT and add Whitenoise for static files

--- codestar_blog/codestar/settings.py
+++ codestar_blog/codestar/settings.py
@@ -62,4 +62,14 @@ USE_I18N = True
 USE_TZ = True
 
 STATIC_URL = '/static/'
+STATIC_ROOT = BASE_DIR / 'staticfiles'
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+
+
+
+
+
+# Optional: let Django serve static files efficiently on Heroku
+STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
+
